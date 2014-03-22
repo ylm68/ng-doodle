8c5622f0f009951c9d641cbbda5fed36732d8708
ylm68 <ylm68@free.fr>
2014-03-22 02:25:34 +0100

    first commit after yo angular # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # Explicit paths specified without -i nor -o; assuming --only paths... # # Committer: philippe le roux <ylm68@air-de-pleroux.bouyguesbox.fr> # # On branch master # Your branch is up-to-date with 'origin/master'. # # Changes to be committed: #	modified:   README #

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('ngDoodleApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
